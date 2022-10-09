5de981d2764bb3d6c9a1ee5acc6942702af867f9
1 <1@DESKTOP-E15E3SS>
2022-10-09 10:10:32 +0300

    Proje dosyası ekle.

---
+++ Example/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool kontrol = true;
+            ogrenci ogrenci1 = new ogrenci(1663,"Baran","Karakaya",45,60,55,"Sakarya Üniversitesi");
+
+            Console.WriteLine("Uygulamaya Hoş Geldiniz. Yapmak İstediğiniz İşlemi Seçiniz.");
+            islemleriGoster();
+            
+
+            while (kontrol)
+            {
+                string secim = Console.ReadLine();
+                switch (secim)
+                {
+                    case "1":
+                        ogrenci1.OgrenciBilgileriGoster();
+                        break;
+                    case "2":
+                        double ogrenciOrtalama = ogrenci1.OgrenciNotuBul();
+                        if (ogrenciOrtalama < 50)
+                        {
+                            Console.WriteLine("Ortalaması Cok Düşük.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ortalaması Gayet Güzel");
+                        }
+                        break;
+                    case "3":
+                        ogrenci1.okulGetir();
+                        break;
+                    case "4":
+                        kontrol = false;
+                        break;
+
+
+                }
+            }
+        }
+
+
+        static void islemleriGoster()
+        {
+            Console.WriteLine("1-Öğrenci Bilgileri Göster");
+            Console.WriteLine("2-Öğrenci Ortalaması Göster");
+            Console.WriteLine("3-Öğrenci Okulunu Göster");
+            Console.WriteLine("4-Cıkış Yap");
+        }
+    }
+}
---
+++ Example/ogrenci.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    public class ogrenci
+    {
+        private int ogrno;
+        private string isim;
+        private string Soyisim;
+        private int vize1;
+        private int vize2;
+        private int final;
+        private string okulIsmi;
+
+         public ogrenci(int _ogrno,string _isim, string _Soyisim,int _vize1,int _vize2, int _final,string _okulIsmi)
+        {
+            ogrno = _ogrno;
+            isim = _isim;
+            Soyisim = _Soyisim;
+            vize1 = _vize1;
+            vize2 = _vize2;
+            final = _final;
+            okulIsmi = _okulIsmi;
+        }
+
+
+        public double OgrenciNotuBul()
+        {
+            double ortalama = vize1 * 0.2 + vize2 * 0.2 + final * 0.6;
+            return ortalama;
+        }
+
+        public void okulGetir()
+        {
+            Console.WriteLine("Öğrencinin Okulu :"+okulIsmi);
+        }
+
+        public void OgrenciBilgileriGoster()
+        {
+            Console.WriteLine("Öğrenci Numarası :"+ogrno);
+            Console.WriteLine("Öğrenci Adı :"+isim);
+            Console.WriteLine("Öğrenci soyadı :"+Soyisim);
+            Console.WriteLine("Öğrenci vize1 Notu :"+vize1);
+            Console.WriteLine("Öğrenci vize2 Notu :"+vize2);
+            Console.WriteLine("Öğrenci final Notu :"+final);
+            Console.WriteLine("Öğrenci okul Adı :"+okulIsmi);
+        }
+    }
+}
